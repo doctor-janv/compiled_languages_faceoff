0bb87b0693d14bf9efb9bd49fed1d7eb9b87dfff
Naktakala <janicvermaak@gmail.com>
2026-04-06 08:05:09 -0600

    Test4 beginning

---
+++ Test4/gpp.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+
+#include <vector>
+#include <string>
+#include <math.h>
+
+#include "../common/disabled_alloc.hpp"
+
+typedef std::vector<double,Alloc<double>> Vector;
+
+void initVector(Vector& vec, int64_t N)
+{
+    vec.resize(N);
+    for (int64_t i=0; i<N; ++i)
+        vec[i] = static_cast<double>(i);
+}
+
+void operation(const Vector& vec, int64_t N, double& sum)
+{
+    sum = 0.0;
+    for (int64_t i=0; i<N; ++i)
+        sum += sqrt(vec[i]);
+}
+
+int main(int argc, char** argv)
+{
+    const std::string N_str = argv[1];
+    
+    const int64_t N = std::stoll(N_str);
+
+    Vector vec;
+    double sum;
+
+    initVector(vec, N);
+    
+    const auto start = std::chrono::steady_clock::now();
+
+    operation(vec, N, sum);
+
+    const auto end = std::chrono::steady_clock::now();
+
+    const auto duration = std::chrono::duration<double>(end - start);
+
+    std::cout << "Elapsed time = " 
+              << std::scientific << std::setprecision(6)
+              << duration.count() << " seconds " << sum << std::endl;
+
+    return 0;
+}
--- ZMakeAll_AMDRyzen5800xOC.sh
+++ ZMakeAll_AMDRyzen5800xOC.sh
@@ -329,4 +329,46 @@ pushd Test3/
     -o bin/icx_w_lu8.x
 popd
 
+pushd Test4/
+    mkdir "bin" 2>/dev/null || true
+
+    gfortran -std=f2008 -O2 -march=native gfortran.F90      -o bin/gfortran.x
+    gfortran -std=f2008 -O3 -march=native gfortran.F90      -o bin/gfortran_o3.x
+
+    g++ -std=c++17 -O2 -march=native gpp.cc      -o bin/gpp.x
+    g++ -std=c++17 -O3 -march=native gpp.cc      -o bin/gpp_o3.x
+
+
+    ifx.exe -nologo /stand:f08 /O2 /QxHost gfortran.F90 /link \
+    /libpath:"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.42.34433\lib\x64" \
+    /libpath:"C:\Program Files (x86)\Intel\oneAPI\compiler\2025.0\lib" \
+    /libpath:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\um\x64" \
+    /libpath:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\ucrt\x64" \
+    /out:bin/ifx.x
+
+    ifx.exe -nologo /stand:f08 /O3 /QxHost gfortran.F90 /link \
+    /libpath:"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.42.34433\lib\x64" \
+    /libpath:"C:\Program Files (x86)\Intel\oneAPI\compiler\2025.0\lib" \
+    /libpath:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\um\x64" \
+    /libpath:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\ucrt\x64" \
+    /out:bin/ifx_o3.x
+
+
+
+    icpx.exe -std=c++17 -O2 -march=native gpp.cc \
+    -L"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.42.34433\lib\x64" \
+    -L"C:\Program Files (x86)\Intel\oneAPI\compiler\2025.3\lib" \
+    -L"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\um\x64" \
+    -L"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\ucrt\x64" \
+    -o bin/icx.x
+
+    icpx.exe -std=c++17 -O3 -march=native gpp.cc \
+    -L"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.42.34433\lib\x64" \
+    -L"C:\Program Files (x86)\Intel\oneAPI\compiler\2025.3\lib" \
+    -L"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\um\x64" \
+    -L"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\ucrt\x64" \
+    -o bin/icx_o3.x
+
+popd
+
 echo "Compilation done"
